3cb28654aed0c2450fd93bfa594d2576f996cab1
camdeun <cameronrobinson492@yahoo.com>
2025-02-17 21:21:05 -0600

    Homepagee.jsx

---
+++ Homepagee.jsx
@@ -0,0 +1,11 @@
+import VideoPlayer from "../components/VideoPlayer"
+
+export default function Homepagee() {
+    return (
+        <div className="Homepagee">
+            <h1>Home</h1>
+            <VideoPlayer />
+        </div>
+    )
+}
+
